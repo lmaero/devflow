c16a050f37acd5dd08af47cb312384f7c836d2cb
Luis Guzman <luis.guzman@falcani.com>
2024-12-10 18:37:47 -0500

    Update dependencies to latest versions
    This commit upgrades multiple dependencies across the project, including Next.js, React, TypeScript, and tailwindcss, among others, to their respective latest versions. These updates improve compatibility, optimize performance, and address any potential vulnerabilities or deprecations.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import { ThemeProvider } from '@/context/Theme'
 import type { ReactNode } from 'react'
 
 const inter = localFont({
@@ -30,11 +31,18 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
