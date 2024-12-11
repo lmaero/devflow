4db09b206550c287c74061b4b9366b95c60e83e4
Luis Guzman <luis.guzman@falcani.com>
2024-12-11 12:35:09 -0500

    Add GitHub authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
-import { Navbar } from '@/components/navigation/navbar'
+import { auth } from '@/auth'
+import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/context/Theme'
+import { SessionProvider } from 'next-auth/react'
 import type { ReactNode } from 'react'
 
 const inter = localFont({
@@ -26,26 +28,31 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode
 }>) {
+  const session = await auth()
+
   return (
     <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   )
 }
