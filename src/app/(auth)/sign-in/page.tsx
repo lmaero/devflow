b39d73314e4beba72a3d01d9cba3b849719dbab0
Luis Guzman <luis.guzman@falcani.com>
2024-12-12 10:59:34 -0500

    Add Auth Form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import { AuthForm } from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validations'
+
 export default function SignIn() {
-  return <div>Sign In</div>
+  return (
+    <AuthForm
+      formType='SIGN_IN'
+      schema={SignInSchema}
+      defaultValues={{ email: '', password: '' }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  )
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import { AuthForm } from '@/components/forms/AuthForm'
+import { SignUpSchema } from '@/lib/validations'
+
 export default function SignUp() {
-  return <div>Sign Up</div>
+  return (
+    <AuthForm
+      formType='SIGN_UP'
+      schema={SignUpSchema}
+      defaultValues={{ email: '', password: '', name: '', username: '' }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  )
 }
