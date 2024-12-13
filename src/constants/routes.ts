d9fcb695aee6062d39b1c319f32d00256e852029
Luis Guzman <luis.guzman@falcani.com>
2024-12-13 11:43:49 -0500

    Add Sidebars

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,4 +2,6 @@ export const ROUTES = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  PROFILE: (id: string) => `/profile/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
+import { techMap } from '@/constants/techMap'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getDeviconClassName(techName: string) {
+  const normalizedTechName = techName.replace(/[ .]/g, '').toLowerCase()
+
+  return techMap[normalizedTechName]
+    ? `${techMap[normalizedTechName]} colored`
+    : 'devicon-devicon-plain'
 }
