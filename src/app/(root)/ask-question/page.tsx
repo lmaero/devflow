b8e42e9ca3d5d72366e029d44ec5270c57b71711
Luis Guzman <luis.guzman@falcani.com>
2025-01-19 10:04:37 -0500

    Implement UI of the Question Form

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,3 +1,15 @@
-export default function AskQuestion() {
-  return <div>Ask Question</div>
+import React from "react";
+
+import { QuestionForm } from "@/components/forms/QuestionForm";
+
+export default function AskAQuestion() {
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+
+      <div className='mt-9'>
+        <QuestionForm />
+      </div>
+    </>
+  )
 }
